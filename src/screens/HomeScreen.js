cb4900ea571b518b85b0d3167ad6d07e0a202966
Ziya <ziyakaragoz42@gmail.com>
2021-12-15 16:53:06 +0300

    deneme

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,12 +1,11 @@
 import React, { Component } from "react";
-import { StyleSheet, View, StatusBar, TouchableOpacity } from "react-native";
+import { StyleSheet, View, StatusBar } from "react-native";
 
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
 } from "react-native-responsive-screen";
 
-//Component imports
 import HeaderBar from "../components/molecules/HeaderBar";
 import FooterBar from "../components/molecules/FooterBar";
 import Icon from "react-native-vector-icons/Entypo";
@@ -15,37 +14,30 @@ function HomeScreen(props) {
   return (
     <View style={styles.container}>
       <StatusBar hidden />
-      <FooterBar style={styles.footerbar}></FooterBar>
       <HeaderBar style={styles.headerBar}></HeaderBar>
-      <TouchableOpacity>
-        <Icon name='circle-with-plus' style={styles.icon}></Icon>
-      </TouchableOpacity>
+      <Icon name='circle-with-plus' style={styles.icon}></Icon>
+      <FooterBar style={styles.footerBar}></FooterBar>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    height: hp("100%"),
-    width: wp("100%"),
-  },
-  footerbar: {
-    height: 75,
-    width: wp("100%"),
-    backgroundColor: "rgba(242,242,242,1)",
-    marginTop: 645,
+    display: "flex",
   },
   headerBar: {
-    height: 75,
-    width: wp("100%"),
-    marginTop: -720,
+    height: hp("10%"),
+  },
+  footerBar: {
+    height: hp("10%"),
+    backgroundColor: "rgba(242,242,242,1)",
+    marginBottom: hp("0%"),
   },
   icon: {
     color: "rgba(128,128,128,1)",
     fontSize: 60,
-    marginTop: 473,
-    marginLeft: 277,
+    marginTop: hp("65%"),
+    marginLeft: wp("77%"),
   },
 });
 
