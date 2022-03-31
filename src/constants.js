6cb07e1990a8a8de40e3057b1ab4e77d37a833d7
karagoz-ziya <sar581999@gmail.com>
2022-03-31 14:04:16 +0300

    .

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
 const localIP = "192.168.1.100";
-const ngrokServer = "https://e1ba-159-146-45-11.ngrok.io";
+const ngrokServer = "https://4ecb-159-146-14-171.ngrok.io";
 
 export { localIP, ngrokServer };
