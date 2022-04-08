7a13812262dced7886bc48720f93e94669faa0a8
karagoz-ziya <sar581999@gmail.com>
2022-04-08 16:18:18 +0300

    a

--- src/components/molecules/FooterBar.js
+++ src/components/molecules/FooterBar.js
@@ -2,12 +2,28 @@ import React from "react";
 import { View, TouchableOpacity, Text } from "react-native";
 import MaterialCommunityIconsIcon from "react-native-vector-icons/MaterialCommunityIcons";
 import { useNavigation } from "@react-navigation/native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 import allStyles from "./Styles";
 
 function FooterBar() {
   const navigation = useNavigation();
   const styles = allStyles;
+  const handleLogout = () => {
+    storeData();
+    navigation.reset({
+      index: 1,
+      routes: [{ name: "Home" }, { name: "Login" }],
+    });
+  };
+  const storeData = async () => {
+    try {
+      const jsonValue = JSON.stringify({});
+      await AsyncStorage.setItem("@user", jsonValue);
+    } catch (e) {
+      // saving error
+    }
+  };
   return (
     <View style={styles.barContainerBottom}>
       <TouchableOpacity
@@ -40,15 +56,7 @@ function FooterBar() {
         ></MaterialCommunityIconsIcon>
         <Text style={styles.iconText}>Ana Sayfa</Text>
       </TouchableOpacity>
-      <TouchableOpacity
-        onPress={() =>
-          navigation.reset({
-            index: 1,
-            routes: [{ name: "Home" }, { name: "Login" }],
-          })
-        }
-        style={styles.btnWrapper}
-      >
+      <TouchableOpacity onPress={handleLogout} style={styles.btnWrapper}>
         <MaterialCommunityIconsIcon
           name='bell-ring'
           style={styles.iconFooter}
