5093f787fde37c9066f4deab0c99b4543d3f2e7d
karagoz-ziya <sar581999@gmail.com>
2022-05-30 13:03:49 +0300

    Update AppSettingScreen.js

--- src/screens/AppSettingScreen.js
+++ src/screens/AppSettingScreen.js
@@ -4,14 +4,15 @@ import { View, Text } from "react-native";
 import FooterBarNav from "../components/molecules/FooterBarNav";
 import HeaderBar from "../components/molecules/HeaderBar";
 
-function AppSettingScreen(){
-    return(
-        <View style={{flex: 8}}>
-            <HeaderBar ></HeaderBar>
-            <View style = {{flex: 6}}>
-                <Text >BURASI UYGULAMA AYALARI</Text>
-            </View>
-            <FooterBarNav></FooterBarNav>
-        </View>
-    )
-} export default AppSettingScreen
+function AppSettingScreen() {
+  return (
+    <View style={{ flex: 8 }}>
+      <HeaderBar></HeaderBar>
+      <View style={{ flex: 6 }}>
+        <Text>BURASI UYGULAMA AYALARI</Text>
+      </View>
+      <FooterBarNav></FooterBarNav>
+    </View>
+  );
+}
+export default AppSettingScreen;
